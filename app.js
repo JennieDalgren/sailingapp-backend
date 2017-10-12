b7968f4dc6953137960e4d7890771dd76e81ce58
Jennie Dalgren <jennie.dalgren@gmail.com>
2017-10-12 20:57:43 +0200

    cleaned code

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const dotenv       = require('dotenv').config();
 const express      = require('express');
 const path         = require('path');
-const favicon      = require('serve-favicon');
 const logger       = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser   = require('body-parser');
@@ -50,8 +49,6 @@ app.use(passport.session());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
